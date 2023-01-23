4b4934a1dc15111e1c17a7690ffad13a823648ba
isaac-ad <isaacadnanu@gmail.com>
2023-01-23 12:56:30 -0500

    Odd checker

---
+++ loopy-lighthouse.js
@@ -0,0 +1,17 @@
+const print =function() {
+    for(let i = 100; i < print; i++) {
+        if (i % 3 === 0 && print % i === 0) {
+            console.log("loopyLighthouse")
+        }
+        else if(i % 4 === 0){
+         console.log("Light-house")
+        }
+        else if (i % 3 === 0) {
+          console.log("loopy")
+        }
+        else {
+        console.log(i)
+        }
+    }
+}
+console.log(print(200));
